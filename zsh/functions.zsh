a8ae8543e5836500ec6fecc69b165bcacf909169
Bastiaan Stroosnijder <bas@strootje.com>
2016-03-16 16:14:07 +0100

    initial setup

---
+++ zsh/functions.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# include colors
+source ./colors.zsh
+
+function progress {
+	printf "\r### ${CYELLOW}[${CRESET} ${CBLUE}..${CRESET} ${CYELLOW}]${CRESET} ::: $1"
+}
+
+function question {
+	printf "\r### ${CYELLOW}[${CRESET} ${CYELLOW}??${CRESET} ${CYELLOW}]${CRESET} ::: $1"
+}
+
+function success {
+	printf "\r### ${CYELLOW}[${CRESET}${CGREEN}DONE${CRESET}${CYELLOW}]${CRESET} ::: $1\n"
+}
+
+function error {
+	printf "\r### ${CYELLOW}[${CRESET}${CRED}FAIL${CRESET}${CYELLOW}]${CRESET} ::: $1\n"
+}
