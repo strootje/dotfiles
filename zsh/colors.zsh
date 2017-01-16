1bd56d65322358918c29b078a57e39217ed0709d
sdfsdf <sdfsdf>
2017-01-16 16:09:54 +0100

    updated installer

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+BASEDIR=$(pwd dirname $0)
 
-BASEPATH=$(dirname $0)
+CRESET="\e[0m"
 
 CRED="\e[1;31m"
+CGRAY="\e[1;30m"
 CGREEN="\e[1;32m"
 CYELLOW="\e[1;33m"
 CBLUE="\e[1;34m"
-CRESET="\e[0m"
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,24 +1,50 @@
 #!/usr/bin/env bash
+BASEDIR=$(pwd dirname $0)
+source $BASEDIR/zsh/colors.zsh
 
-# include colors
-source $DOTFILES/zsh/colors.zsh
+function joinstr {
+	local IFS="$1";
+	shift;
+	echo "$*";
+}
+
+function clean {
+	printf "\r                                                                                                                                                "
+}
 
 function info {
-	printf "\r### ${CYELLOW}[${CRESET} ${CBLUE}..${CRESET} ${CYELLOW}]${CRESET} ::: $1\n"
+	clean
+	printf "\r   ${CYELLOW}[${CRESET}${CGRAY} .. ${CRESET}${CYELLOW}]${CRESET} $1\n"
 }
 
 function progress {
-	printf "\r### ${CYELLOW}[${CRESET} ${CBLUE}..${CRESET} ${CYELLOW}]${CRESET} ::: $1"
+	clean
+	printf "\r   ${CYELLOW}[${CRESET}${CGRAY} .. ${CRESET}${CYELLOW}]${CRESET} $1"
+	sleep .05
 }
 
 function question {
-	printf "\r### ${CYELLOW}[${CRESET} ${CYELLOW}??${CRESET} ${CYELLOW}]${CRESET} ::: $1"
+	clean
+	printf "\r   ${CYELLOW}[${CRESET}${CYELLOW} ?? ${CRESET}${CRESET}${CYELLOW}]${CRESET} $1 "
+	read -r -e REPLY < /dev/tty
+}
+
+function confirm {
+	clean
+	printf "\r   ${CYELLOW}[${CRESET}${CYELLOW} ?? ${CRESET}${CRESET}${CYELLOW}]${CRESET} $1 [yn] "
+	read -n 1 -r -i "Y" REPLY < /dev/tty
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		return 1
+	fi
 }
 
 function success {
-	printf "\r### ${CYELLOW}[${CRESET}${CGREEN}DONE${CRESET}${CYELLOW}]${CRESET} ::: $1\n"
+	clean
+	printf "\r   ${CYELLOW}[${CRESET}${CGREEN}DONE${CRESET}${CYELLOW}]${CRESET} $1\n"
 }
 
 function error {
-	printf "\r### ${CYELLOW}[${CRESET}${CRED}FAIL${CRESET}${CYELLOW}]${CRESET} ::: $1\n"
+	clean
+	printf "\r   ${CYELLOW}[${CRESET}${CRED}FAIL${CRESET}${CYELLOW}]${CRESET} $1\n"
+	exit 1
 }
