75cf7cde69870143179b31be4d9062cc5f1c99a0
Bas Stroosnijder <bas@strootje.com>
2019-01-10 06:42:13 +0100

    updated dotfiles

--- .config/omf/init.fish
+++
@@ -1,8 +0,0 @@
-# Setup Dotfiles
-export DOTFILES=$HOME/.dotfiles
-alias dgit='git --git-dir=$DOTFILES/.git --work-tree=$HOME'
-
-# Setup PATH
-# export PATH=$PATH:$DOTFILES/bin
-
-# Setup Default Editor
