b65fb293471a346fbe5314ff39388e7b60c768de
Bas Stroosnijder <bas@strootje.com>
2019-03-11 21:45:30 +0100

    revert weird commit

---
+++ .config/omf/init.fish
@@ -0,0 +1,8 @@
+# Setup Dotfiles
+export DOTFILES=$HOME/.dotfiles
+alias dgit='git --git-dir=$DOTFILES/.git --work-tree=$HOME'
+
+# Setup PATH
+# export PATH=$PATH:$DOTFILES/bin
+
+# Setup Default Editor
