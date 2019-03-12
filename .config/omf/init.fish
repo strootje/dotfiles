19e13ac1ea28ce28900bc2c814f1eb0cf4cdde4a
Bas Stroosnijder <bas@strootje.com>
2019-03-12 07:00:56 +0100

    updated submodules, ff, fish

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,8 +1,10 @@
 # Setup Dotfiles
 export DOTFILES=$HOME/.dotfiles
+set PATH $PATH $DOTFILES/bin
 alias dgit='git --git-dir=$DOTFILES/.git --work-tree=$HOME'
 
-# Setup PATH
-# export PATH=$PATH:$DOTFILES/bin
+# Setup NodeJS
+set PATH $PATH $HOME/
 
 # Setup Default Editor
+set EDITOR vim
